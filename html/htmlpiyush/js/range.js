8894e1b3e55590ba7cfaa12ebc786730609d3a77
root <root@piyush.expert.com>
2018-02-06 01:50:12 +0530

    html files

---
+++ html/htmlpiyush/js/range.js
@@ -0,0 +1,24 @@
+var rangeSlider = function(){
+  var slider = $('.range-slider'),
+      range = $('.range-slider__range'),
+      value = $('.range-slider__value');
+    
+  slider.each(function(){
+
+    value.each(function(){
+      var value = $(this).prev().attr('value');
+      $(this).html(value);
+    });
+
+    range.on('input', function(){
+      $(this).next(value).html(this.value);
+    });
+  });
+};
+
+rangeSlider();
+
+
+
+
+9680298766  Rahul sir
